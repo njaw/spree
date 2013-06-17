f1e405274910295185bd69c38d3f08039ae6be14
Ryan Bigg <radarlistener@gmail.com>
2013-06-17 14:48:38 +1000

    Fix process of adding line items to an existing order

--- app/assets/javascripts/spree/apps/cart/cart_controller.js
+++ app/assets/javascripts/spree/apps/cart/cart_controller.js
@@ -3,27 +3,28 @@ SpreeStore.module('Cart',function(Cart, SpreeStore, Backbone,Marionette,$,_){
     showCartInfo: function() {
       if (SpreeStore.current_order_id !== undefined) {
         model = new SpreeStore.Models.Order({ 
-          id: SpreeStore.current_order_id,
-          total_quantity: window.localStorage['total_quantity'],
-          display_item_total: window.localStorage['display_item_total']
+          id: SpreeStore.current_order_id
         })
         var cart_info_view = new Cart.CartInfoView({
           model: model
         })
-
-        SpreeStore.cartInfo.show(cart_info_view)
+        model.fetch({
+          data: $.param({ order_token: SpreeStore.current_order_token}),
+          success: function(data) {
+            SpreeStore.cartInfo.show(cart_info_view)
+          }
+        })
       }
     },
     addToCart: function(id, quantity) {
       if (SpreeStore.current_order_id === undefined) {
         Cart.Controller.newCheckout(id, quantity)
       } else {
-        Cart.Controller.updateOrder(id, quantity)
+        Cart.Controller.addLineItem(id, quantity)
       }
     },
 
     newCheckout: function(id, quantity) {
-      // TODO: I wonder if this could be moved into Backbone-esque code...
       $.ajax({
         type: 'POST', 
         url: '/store/api/orders',
@@ -38,13 +39,16 @@ SpreeStore.module('Cart',function(Cart, SpreeStore, Backbone,Marionette,$,_){
           }
         },
         success: function(data) {
+          window.localStorage['current_order_token'] = SpreeStore.current_order_token = data.token
+          window.localStorage['current_order_id'] = SpreeStore.current_order_id = data.number
           Cart.Controller.updateCart(data)
         }
       })
     },
 
-    updateOrder: function(id, quantity) {
-      $.post({
+    addLineItem: function(id, quantity) {
+      $.ajax({
+        type: 'post',
         url: '/store/api/orders/' + SpreeStore.current_order_id + '/line_items',
         data: {
           order_token: SpreeStore.current_order_token,
@@ -54,21 +58,12 @@ SpreeStore.module('Cart',function(Cart, SpreeStore, Backbone,Marionette,$,_){
           }
         },
         success: function(data) {
-          Cart.Controller.updateCart(data)
+          Cart.Controller.updateCart()
         }
       })
     },
 
     updateCart: function(data) {
-      window.localStorage['current_order_token'] = SpreeStore.current_order_token = data.token
-      window.localStorage['current_order_id'] = SpreeStore.current_order_id = data.number
-      window.localStorage['total_quantity'] = data.total_quantity
-      window.localStorage['display_item_total'] = data.display_item_total
-      
-      model = new SpreeStore.Models.Order(data)
-      cart_info_view = new Cart.CartInfoView({
-        model: model
-      })
       Cart.Controller.showCartInfo()
       Cart.Controller.preview()
     },
