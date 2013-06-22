f5f287e41b9419fa8365a5477fa84575222a8236
Ryan Bigg <radarlistener@gmail.com>
2013-06-22 17:54:28 +1000

    Ensure no sidebar on order page

--- app/assets/javascripts/spree/apps/order/order_controller.js
+++ app/assets/javascripts/spree/apps/order/order_controller.js
@@ -1,6 +1,7 @@
 SpreeStore.module('Order',function(Order, SpreeStore, Backbone,Marionette,$,_){
   Order.Controller = {
     show: function() {
+      SpreeStore.noSidebar()
       order = new SpreeStore.Models.Order({ number: SpreeStore.current_order_id })
       order.fetch({
         data: $.param({ order_token: SpreeStore.current_order_token}),
