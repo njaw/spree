6c8d2b8d626d1d893e2a05cced86f06b07e96ae9
Ryan Bigg <radarlistener@gmail.com>
2013-07-01 15:42:28 +1000

    Begin work on payments screen of checkout

---
+++ app/assets/javascripts/spree/apps/order/payment_view.js
@@ -0,0 +1,36 @@
+SpreeStore.module('Order',function(Order, SpreeStore, Backbone,Marionette,$,_){
+  Order.paymentView = Backbone.Marionette.ItemView.extend({
+    template: '#order-payment-template',
+
+    events: {
+      "submit .edit_order": "updateOrder",
+    },
+
+    updateOrder: function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      $(e.target).find("input[type='submit']").slideUp(function() {
+        $('#checkout #loading').show();  
+      });
+      // if (this.validate()) {
+      //   // TODO: Doing it this way because I don't know how to deal with nested attrs in Backbone
+      //   var data = Backbone.Syphon.serialize(this)
+      //   data['order_token'] = SpreeStore.current_order_token
+      //   $.ajax({
+      //     type: 'PUT',
+      //     dataType: 'json',
+      //     url: '/store/api/checkouts/' + this.model.attributes.number,
+      //     data: data,
+      //     success: function(data) {
+      //       var order = new SpreeStore.Models.Order(data)
+      //       SpreeStore.Order.Controller.renderFor(order)
+      //     },
+      //   })
+      // }
+    },
+
+    validate: function() {
+      return true;
+    }
+  })
+})
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < Spree::StoreController
   layout 'spree/layouts/spree_application'
   def index
-    render :index
+    @payment_method_types = Spree::PaymentMethod.select("DISTINCT type, deleted_at").all.map(&:method_type)
   end
 end
