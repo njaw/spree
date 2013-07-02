4a1b8f63a1e2e28310e6d7cccd278feef92dc142
Ryan Bigg <radarlistener@gmail.com>
2013-07-02 12:27:49 +1000

    Actually update things on the payment screen

--- app/assets/javascripts/spree/apps/order/payment_view.js
+++ app/assets/javascripts/spree/apps/order/payment_view.js
@@ -12,21 +12,24 @@ SpreeStore.module('Order',function(Order, SpreeStore, Backbone,Marionette,$,_){
       $(e.target).find("input[type='submit']").slideUp(function() {
         $('#checkout #loading').show();  
       });
-      // if (this.validate()) {
-      //   // TODO: Doing it this way because I don't know how to deal with nested attrs in Backbone
-      //   var data = Backbone.Syphon.serialize(this)
-      //   data['order_token'] = SpreeStore.current_order_token
-      //   $.ajax({
-      //     type: 'PUT',
-      //     dataType: 'json',
-      //     url: '/store/api/checkouts/' + this.model.attributes.number,
-      //     data: data,
-      //     success: function(data) {
-      //       var order = new SpreeStore.Models.Order(data)
-      //       SpreeStore.Order.Controller.renderFor(order)
-      //     },
-      //   })
-      // }
+      if (this.validate()) {
+        // TODO: Doing it this way because I don't know how to deal with nested attrs in Backbone
+        var data = Backbone.Syphon.serialize(this)
+        data['order_token'] = SpreeStore.current_order_token
+        $.ajax({
+          type: 'PUT',
+          dataType: 'json',
+          url: '/store/api/checkouts/' + this.model.attributes.number,
+          data: data,
+          success: function(data) {
+            var order = new SpreeStore.Models.Order(data)
+            SpreeStore.Order.Controller.renderFor(order)
+          },
+          error: function(xhr) {
+            alert(xhr.responseText)
+          }
+        })
+      }
     },
 
     validate: function() {
